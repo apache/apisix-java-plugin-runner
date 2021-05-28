d9c7faeeb90f02853fb8ecfd4194729a695b58b2
tzssangglass <tzssangglass@gmail.com>
2021-05-28 20:13:39 +0800

    add sample demo code

---
+++ sample/src/main/java/org/apache/apisix/plugin/runner/filter/RewriteRequestDemoFilter.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.apisix.plugin.runner.filter;
+
+import org.apache.apisix.plugin.runner.HttpRequest;
+import org.apache.apisix.plugin.runner.HttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+public class RewriteRequestDemoFilter implements PluginFilter {
+
+    @Override
+    public String name() {
+        /* It is recommended to keep the name of the filter the same as the class name.
+         Configure the filter to be executed on apisix's routes in the following format
+
+        {
+            "uri": "/hello",
+            "plugins": {
+                "ext-plugin-pre-req": {
+                    "conf": [{
+                        "name": "RewriteRequestDemoFilter",
+                        "value": "bar"
+                    }]
+                }
+            },
+            "upstream": {
+                "nodes": {
+                    "127.0.0.1:1980": 1
+                },
+                "type": "roundrobin"
+            }
+        }
+
+        The value of name in the configuration corresponds to the value of return here.
+         */
+
+        return "RewriteRequestDemoFilter";
+    }
+
+    @Override
+    public Mono<Void> filter(HttpRequest request, HttpResponse response, PluginFilterChain chain) {
+        // note: the path to the rewrite must start with '/'
+        request.setPath("/get");
+        request.setHeader("new-header", "header_by_runner");
+        /* note: The value of the parameter is currently a string type.
+                 If you need the json type, you need the upstream service to parse the string value to json.
+                 For example, if the arg is set as follows
+                 request.setArg("new arg", "{\"key1\":\"value1\",\"key2\":2}");
+
+                 The arg received by the upstream service will be as follows
+                 "new arg": "{\"key1\":\"value1\",\"key2\":2}"
+         */
+        request.setArg("new arg", "{\"key1\":\"value1\",\"key2\":2}");
+
+        return chain.filter(request, response);
+    }
+
+    @Override
+    public int getOrder() {
+        //The order of filter execution in runner is determined by the order here, the smaller the order number, the higher the execution order.
+        return 0;
+    }
+}
